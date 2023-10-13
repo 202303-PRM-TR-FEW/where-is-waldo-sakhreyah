6808549dfe6c2e9c9c6f8890928170589da5da32
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-10-13 14:39:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"]];
 
 const whereIsWaldo = (matrix) => {
-  
+  let arr = []
+  let notWaldo
+  if ((matrix[0][0] === matrix[0][1]) || (matrix[0][0] === matrix[1][0])) {
+    notWaldo = matrix[0][0]
+  } else { notWaldo = matrix[0][1] }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+
+      switch (notWaldo) {
+        case matrix[i][j]:
+          break
+        default:
+          arr.push(i + 1)
+          arr.push(j + 1)
+      }
+    }
+  }
+  return arr
 }
+console.log(whereIsWaldo(example))
